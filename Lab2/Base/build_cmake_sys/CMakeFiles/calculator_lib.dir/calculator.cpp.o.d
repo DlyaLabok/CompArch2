29b505cab00bcabf6adb0441a2585a483f5a0e23
ivanka <teteruk.ivanna@lll.kpi.ua>
2026-01-11 21:54:47 +0200

    Delete Lab2 directory

--- Lab2/Base/build_cmake_sys/CMakeFiles/calculator_lib.dir/calculator.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/calculator_lib.dir/calculator.cpp.o: \
- /home/io31teteruk/Lab2_Work/Base/calculator.cpp \
- /usr/include/stdc-predef.h /home/io31teteruk/Lab2_Work/Base/calculator.h
--- Lab2/Base/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "calculator.h"
-int main() {
-    Calculator calc;
-    std::cout << "3.0 + 4.0 = " << calc.Add(3.0, 4.0) << std::endl;
-    std::cout << "3.0 - 4.0 = " << calc.Sub(3.0, 4.0) << std::endl;
-    return 0;
-}
